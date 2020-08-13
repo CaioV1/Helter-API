4e3daf946df401392e16a736e7e04a04c0716bcd
Caio Morais <caiomorais@MacBook-Air-de-Caio.local>
2020-08-12 22:15:58 -0300

    Adding a subdocument structure in mongoose

--- model/recordModel.js
+++ model/recordModel.js
@@ -2,12 +2,19 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const bandSchema = new Schema({
+
+    _id: { type: Schema.Types.ObjectId, ref: "bandSchema", required: false, name: String},
+    name : {type: String, required: true}
+
+});
+
 const recordSchema = new Schema({
 
     _recordId: Schema.Types.ObjectId,
     title: {type: String, required: true},
     artist: { type: Schema.Types.ObjectId, ref: "artistSchema", required: false},
-    band: { type: Schema.Types.ObjectId, ref: "bandSchema", required: false},
+    band: bandSchema,
     launch_date: {type: Date, required: true},
     pathImage: {type: String, required: true},
     created_at: {type: Date, default: Date.now},
