e9751c723817f29629b31d907208f2aec0fcf40e
CaioV1 <caiovictor2014@hotmail.com>
2023-12-06 21:41:57 -0300

    Refactor config files and generate new lock file

--- src/config/DatabaseConnection.js
+++ src/config/DatabaseConnection.js
@@ -1,32 +1,21 @@
 const mongoose = require("mongoose");
 
 class DatabaseConnection {
+  constructor() {
+    this.host = process.env.MONGO_HOST;
+    this.port = process.env.MONGO_PORT;
+    this.database = process.env.MONGO_DATABASE;
+    this.connectionString = `mongodb://${this.host}:${this.port}/${this.database}`
+  }
 
-    constructor(){
-
-        this.host = process.env.MONGO_HOST;
-        this.port = process.env.MONGO_PORT;
-        this.database = process.env.MONGO_DATABASE;
-        this.connectionString = `mongodb://${this.host}:${this.port}/${this.database}`
-
+  async connectDatabase() {
+    try {
+      this.connection = await mongoose.connect(this.connectionString);
+    } catch (error) {
+      console.log("Erro na conexão com o banco")
+      console.log(error)
     }
-
-    async connectDatabase(){
-
-        try {
-
-            this.connection = await mongoose.connect(this.connectionString, {useNewUrlParser : true});
-            //this.connection = mongoose.connection;
-            
-        } catch (error) {
-
-            console.log("Erro na conexão com o banco")
-            console.log(error)
-            
-        }
-
-    }
-
+  }
 }
 
 module.exports = DatabaseConnection;
--- src/config/Server.js
+++ src/config/Server.js
@@ -1,69 +1,37 @@
-require("dotenv").config({
-
-    path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
-
-});
-
-//Módulo com recursos para auxiliar o desenvolvimento de aplicações WEB
+require("dotenv").config({ path: process.env.NODE_ENV === "test" ? ".env.test" : ".env" });
 const express = require("express");
-//Módulo de conversão do corpo de uma requisição
-const bodyParser = require("body-parser");
-//Módulo para prover permissão de acesso
 const cors = require("cors");
-//Módulo de conexão com o banco de dados Mongo
+
+const corsConfig = require('../middleware/corsConfig.js');
 const DatabaseConnection = require("./DatabaseConnection");
 
 class Server {
+  constructor() {
+    this.app = express();
+    this.setupMiddleware();
+    this.setupRoutes();
+  }
 
-    constructor() {
+  setupMiddleware(){
+    this.app.use(cors());
+    this.app.use(express.static(__dirname.replace("/src/config", "") + "/public"));
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+    this.app.use(corsConfig);
 
-        this.app = express();
+    const databaseConnection = new DatabaseConnection();
+    databaseConnection.connectDatabase();
+  }
 
-        this.setupMiddleware();
-        this.setupRoutes();
-
-    }
-
-    setupMiddleware(){
-
-        this.app.use(cors());
-        this.app.use(express.static(__dirname.replace("/src/config", "") + "/public"));
-
-        //Prepara a aplicação para receber o corpo das requisições no formato x-www-form-urlencoded
-        this.app.use(bodyParser.urlencoded({ extended: true }));
-        //Prepara a aplicação para receber o corpo das requisições no formato JSON
-        this.app.use(bodyParser.json());
-
-        this.app.use((req, res, next) => {
-
-            res.setHeader("Access-Control-Allow-Origin" , "*");
-            res.setHeader("Access-Control-Allow-Methods" , "POST, PUT, GET, DELETE");
-            res.setHeader("Access-Control-Allow-Headers" , "Content-Type");
-            res.setHeader("Access-Control-Allow-Credentials" , true);
-        
-            next();
-        
-        });
-
-        const databaseConnection = new DatabaseConnection();
-
-        databaseConnection.connectDatabase();
-
-    }
-
-    setupRoutes(){
-
-        //Usando módulo com as rotas da API
-        this.app.use(
-            require("../routes/userRoute.js").router,
-            require("../routes/audioRoute.js").router,
-            require("../routes/bandRoute.js").router,
-            require("../routes/artistRoute.js").router,
-            require("../routes/recordRoute.js").router
-        );
-
-    }
-
+  setupRoutes(){
+    this.app.use(
+      require("../routes/userRoute.js").router,
+      require("../routes/audioRoute.js").router,
+      require("../routes/bandRoute.js").router,
+      require("../routes/artistRoute.js").router,
+      require("../routes/recordRoute.js").router
+    );
+  }
 }
 
 module.exports = new Server();
